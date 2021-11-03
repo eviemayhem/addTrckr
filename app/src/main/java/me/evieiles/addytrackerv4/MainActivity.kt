fe6d3b4b32b22de7bee5fc49e144c17a9f4106e3
Evie <evie@syntxcoding.org>
2021-11-02 20:53:55 -0400

    Worked on adding in help menu (orange question mark) as well as medication log page.

--- app/src/main/java/me/evieiles/addytrackerv4/MainActivity.kt
+++ app/src/main/java/me/evieiles/addytrackerv4/MainActivity.kt
@@ -1,6 +1,8 @@
 package me.evieiles.addytrackerv4
 
+import android.app.Dialog
 import android.os.Bundle
+import android.widget.ImageView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -8,10 +10,13 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import me.evieiles.addytrackerv4.databinding.ActivityMainBinding
+import me.evieiles.addytrackerv4.databinding.HelpDialogBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var btnHelp: ImageView;
+    private lateinit var dialog: Dialog;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +25,21 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val navView: BottomNavigationView = binding.navView
+        //Sets questionmark icon as button to trigger help popup.
+        btnHelp = findViewById<ImageView>(R.id.btnHelp)
+
+        //Help Popup
+        btnHelp.setOnClickListener{
+            dialog =  Dialog(this);
+            dialog.setContentView(R.layout.help_dialog)
+            dialog.show()
+        }
+
+
+
+
+
+
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
@@ -31,5 +51,12 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+
+
     }
-}
+
+
+    }
+
+
